b73d9b9b6e813942e78ddd8bd0628b83e812f840
Juan Fernandez <jfernan123@ualberta.ca>
2022-02-27 22:56:08 -0700

    Added profile layout activity

--- app/src/main/java/com/example/hashhunter/ProfileActivity.java
+++ app/src/main/java/com/example/hashhunter/ProfileActivity.java
@@ -1,14 +1,27 @@
 package com.example.hashhunter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    int columns = 2;
+    //Set QR list, adapter, and grid manager
+    private RecyclerView QRList;
+    private RecyclerView.Adapter QRAdapter;
+    private GridLayoutManager QRGridManager = new GridLayoutManager(this, columns);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        //Set the layout manager
+        QRList.setLayoutManager(QRGridManager);
+
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/hashhunter/QRItem.java
@@ -0,0 +1,18 @@
+package com.example.hashhunter;
+
+public class QRItem {
+    private int treePic;
+    private String pointText;
+    private String qrName;
+
+    public QRItem(int tree, String points, String name) {
+        treePic = tree;
+        pointText = points;
+        qrName = name;
+
+    }
+
+
+
+
+}
