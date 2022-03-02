0542ce3b8236d641ba636b16a6fc73196fedc37c
Juan Fernandez <jfernan123@ualberta.ca>
2022-03-02 14:35:33 -0700

    Added adapter

--- app/src/main/java/com/example/hashhunter/ProfileActivity.java
+++ app/src/main/java/com/example/hashhunter/ProfileActivity.java
@@ -4,10 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import java.util.ArrayList;
 
 public class ProfileActivity extends AppCompatActivity {
 
+
+    private RecyclerView qrTreeRecycler;
+    private RecyclerView.Adapter qrTreeAdapter;
     int columns = 2;
     //Set QR list, adapter, and grid manager
     private RecyclerView QRList;
@@ -19,6 +25,12 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
         //Set the layout manager
         QRList.setLayoutManager(QRGridManager);
+
+        ArrayList<QRItem> qrList =  new ArrayList<>();
+
+
+        qrList.add(new QRItem(R.drawable.ic_launcher_background, 1, "Potato QR" ));
+        qrList.add(new QRItem(R.drawable.ic_android, 1, "Tomato QR" ));
 
 
 
--- app/src/main/java/com/example/hashhunter/QRItem.java
+++ app/src/main/java/com/example/hashhunter/QRItem.java
@@ -2,14 +2,25 @@ package com.example.hashhunter;
 
 public class QRItem {
     private int treePic;
-    private String pointText;
+    private int qrPoints;
     private String qrName;
 
-    public QRItem(int tree, String points, String name) {
+    public QRItem(int tree, int points, String name) {
         treePic = tree;
-        pointText = points;
+        qrPoints = points;
         qrName = name;
 
+    }
+
+    public int getImageRsrc() {
+        return treePic;
+    }
+
+    public int getQrPoints(){
+        return qrPoints;
+    }
+    public String getQrName() {
+        return qrName;
     }
 
 
