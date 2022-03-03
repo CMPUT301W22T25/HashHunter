d171ef23973dd1ce8fbb9fa0e61cfdbc24dc1e9c
Juan Fernandez <jfernan123@ualberta.ca>
2022-03-02 20:35:30 -0700

    Added profile picture, made point thingies look fancy, fixed recycler view stuff

--- app/src/main/java/com/example/hashhunter/ProfileActivity.java
+++ app/src/main/java/com/example/hashhunter/ProfileActivity.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import java.util.ArrayList;
 
@@ -15,7 +16,7 @@ public class ProfileActivity extends AppCompatActivity {
     int columns = 2;
     //Set QR list, adapter, and grid manager
     private RecyclerView QRRecycler;
-
+    private ImageView profilePic;
     private RecyclerView.Adapter QRRecycleAdapter;
     private GridLayoutManager QRGridManager = new GridLayoutManager(this, columns);
     @Override
@@ -24,14 +25,27 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
         //Set the layout manager
 
-        ArrayList<QRItem> qrList =  new ArrayList<>();
+        //Do profile pic
+        profilePic = findViewById(R.id.profilePicture);
 
+        profilePic.setImageResource(R.drawable.ic_android);
+        ArrayList<QRItem> qrList =  new ArrayList<>();
 
         qrList.add(new QRItem(R.drawable.ic_launcher_background, "420 points", "Potato QR" ));
         qrList.add(new QRItem(R.drawable.ic_android, "360 points", "Tomato QR" ));
         qrList.add(new QRItem(R.drawable.ic_baseline_account_tree_24, "69 points", "Tomato QR" ));
 
+        qrList.add(new QRItem(R.drawable.ic_baseline_child_care_24, "369 points", "Tomacco QR" ));
+        qrList.add(new QRItem(R.drawable.ic_baseline_face_24, "369 points", "Tomacco QR" ));
+        qrList.add(new QRItem(R.drawable.ic_baseline_4g_plus_mobiledata_24, "4g points", "Illuminati QR" ));
 
+        qrList.add(new QRItem(R.drawable.ic_launcher_background, "420 points", "Potato QR" ));
+        qrList.add(new QRItem(R.drawable.ic_android, "360 points", "Tomato QR" ));
+        qrList.add(new QRItem(R.drawable.ic_baseline_account_tree_24, "69 points", "Tomato QR" ));
+
+        qrList.add(new QRItem(R.drawable.ic_baseline_child_care_24, "369 points", "Tomacco QR" ));
+        qrList.add(new QRItem(R.drawable.ic_baseline_face_24, "369 points", "Tomacco QR" ));
+        qrList.add(new QRItem(R.drawable.ic_baseline_4g_plus_mobiledata_24, "4g points", "Illuminati QR"));
         QRRecycler = findViewById(R.id.treeList);
 
         QRRecycler.setLayoutManager(QRGridManager);
