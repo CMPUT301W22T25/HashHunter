1d0954db4aef4100ed632647233a507d05c49a0b
Dimas Sudjito <almuhtad@ualberta.ca>
2022-03-03 17:23:42 -0700

    connect explore button to explore activity

--- app/src/main/java/com/example/hashhunter/DashboardActivity.java
+++ app/src/main/java/com/example/hashhunter/DashboardActivity.java
@@ -2,7 +2,10 @@ package com.example.hashhunter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -10,5 +13,37 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        initializeActivity();
+    }
+
+    private void initializeActivity() {
+        final Button exploreButton = (Button) findViewById(R.id.button_explore);
+        final Button scanButton = (Button) findViewById(R.id.button_scan);
+        final Button mapButton = (Button) findViewById(R.id.button_map);
+        final Button profileButton = (Button) findViewById(R.id.button_profile);
+        exploreButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(DashboardActivity.this, ExploreActivity.class);
+                DashboardActivity.this.startActivity(intent);
+            }
+        });
+        scanButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(DashboardActivity.this, ExploreActivity.class);
+                DashboardActivity.this.startActivity(intent);
+            }
+        });
+        exploreButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(DashboardActivity.this, ExploreActivity.class);
+                DashboardActivity.this.startActivity(intent);
+            }
+        });
+        exploreButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(DashboardActivity.this, ExploreActivity.class);
+                DashboardActivity.this.startActivity(intent);
+            }
+        });
     }
 }
