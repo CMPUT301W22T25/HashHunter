75932abec9577352f3991439bf7bff6fe9770f60
Dimas Sudjito <almuhtad@ualberta.ca>
2022-03-10 12:27:15 -0700

    sucessfuly take picture and display in on app

--- app/src/main/java/com/example/hashhunter/MainActivity.java
+++ app/src/main/java/com/example/hashhunter/MainActivity.java
@@ -26,9 +26,9 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
-
-    public void launchDashboardActivity(View v) {
-        Intent intent = new Intent(this, DashboardActivity.class);
+    // DEBUG while login is under construction
+    public void launchTempActivity(View v) {
+        Intent intent = new Intent(this, CameraActivity.class);
         startActivity(intent);
     }
 }
