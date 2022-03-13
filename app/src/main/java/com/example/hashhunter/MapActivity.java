4723f5f97a1f141c6d95484e9f54c50ba71cf77d
eggmangrg <aagaman@ualberta.ca>
2022-03-13 01:05:40 -0700

    See player on the map and can interact with map (added Google Maps api)

--- app/src/main/java/com/example/hashhunter/MapActivity.java
+++ app/src/main/java/com/example/hashhunter/MapActivity.java
@@ -6,26 +6,34 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.libraries.maps.CameraUpdateFactory;
+import com.google.android.libraries.maps.GoogleMap;
+import com.google.android.libraries.maps.OnMapReadyCallback;
+import com.google.android.libraries.maps.SupportMapFragment;
+import com.google.android.libraries.maps.model.LatLng;
+import com.google.android.libraries.maps.model.MarkerOptions;
+
 public class MapActivity extends AppCompatActivity {
     //https://www.youtube.com/watch?v=l-J6gDYtgFU
     //Get GPS location
     private LocationManager locationManager;
-    private TextView latitude;
-    private TextView longitude;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private SupportMapFragment supportMapFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
-        latitude = findViewById(R.id.latitude);
-        longitude = findViewById(R.id.longitude);
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.google_map);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
         if(ContextCompat.checkSelfPermission(MapActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED&&
         ContextCompat.checkSelfPermission(MapActivity.this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(MapActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION,},1);
@@ -34,8 +42,16 @@ public class MapActivity extends AppCompatActivity {
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10, 1, new LocationListener() {
             @Override
             public void onLocationChanged(@NonNull Location location) {
-                longitude.setText(String.valueOf(location.getLongitude()));
-                latitude.setText(String.valueOf(location.getLatitude()));
+                supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+                    @Override
+                    public void onMapReady(GoogleMap googleMap) {
+                        LatLng latlng = new LatLng(location.getLatitude(),location.getLongitude());
+                        MarkerOptions options = new MarkerOptions().position(latlng).title("Player");
+                        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latlng,10));
+                        googleMap.addMarker(options);
+
+                    }
+                });
             }
         });
 
