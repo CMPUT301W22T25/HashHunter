4952e00c5215bdf328ab4d22fcf9cb083d9b7ce5
eggmangrg <aagaman@ualberta.ca>
2022-03-12 20:14:24 -0700

    able to see current location now

--- app/src/main/java/com/example/hashhunter/MapActivity.java
+++ app/src/main/java/com/example/hashhunter/MapActivity.java
@@ -1,95 +1,43 @@
 package com.example.hashhunter;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-
-import android.Manifest;
-import android.app.Activity;
-import android.content.pm.PackageManager;
-import android.location.Address;
-import android.location.Geocoder;
-import android.location.Location;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-
-import java.io.IOException;
-import java.util.List;
-import java.util.Locale;
+import androidx.core.content.ContextCompat;
 
 public class MapActivity extends AppCompatActivity {
-    Button getLocationButton;
-    TextView address;
-    TextView longitude;
-    TextView latitude;
-    TextView locality;
-    TextView country;
-    FusedLocationProviderClient fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-
+    //https://www.youtube.com/watch?v=l-J6gDYtgFU
+    //Get GPS location
+    private LocationManager locationManager;
+    private TextView latitude;
+    private TextView longitude;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
-
-        getLocationButton = findViewById(R.id.locationButton);
-        address = findViewById(R.id.address);
-        locality = findViewById(R.id.locality);
         latitude = findViewById(R.id.latitude);
         longitude = findViewById(R.id.longitude);
-        country = findViewById(R.id.country);
-        getLocationButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (ActivityCompat.checkSelfPermission(MapActivity.this
-                        , Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                    getLocation();
-
-                } else {
-                    ActivityCompat.requestPermissions(MapActivity.this,
-                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
-                }
-            }
-        });
-
-    }
-
-    private void getLocation() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if(ContextCompat.checkSelfPermission(MapActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED&&
+        ContextCompat.checkSelfPermission(MapActivity.this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MapActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION,},1);
         }
-        fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
-            @Override
-            public void onComplete(@NonNull Task<Location> task) {
-                Location location = task.getResult();
-                if (location != null) {
-                    try {
-                        Geocoder geocoder = new Geocoder(MapActivity.this, Locale.getDefault());
-                        List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
-                        address.setText("" + addresses.get(0).getAddressLine(0));
-                        country.setText("" + addresses.get(0).getCountryName());
-                        latitude.setText("" + addresses.get(0).getLatitude());
-                        longitude.setText("" + addresses.get(0).getLongitude());
-                        locality.setText("" + addresses.get(0).getLocality());
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
 
-                }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10, 1, new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                longitude.setText(String.valueOf(location.getLongitude()));
+                latitude.setText(String.valueOf(location.getLatitude()));
             }
         });
+
     }
 }
