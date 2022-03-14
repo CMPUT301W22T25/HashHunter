64975ab85c947d987b35335fe0925057ed42c20c
eggmangrg <aagaman@ualberta.ca>
2022-03-13 22:37:07 -0600

    Map comments

--- app/src/main/java/com/example/hashhunter/MapActivity.java
+++ app/src/main/java/com/example/hashhunter/MapActivity.java
@@ -13,8 +13,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.libraries.maps.CameraUpdateFactory;
@@ -33,21 +31,18 @@ public class MapActivity extends AppCompatActivity {
      * @References GPS location app: https://www.youtube.com/watch?v=l-J6gDYtgFU Google Maps fragment: https://www.youtube.com/watch?v=p0PoKEPI65o
      *
      */
-    //https://www.youtube.com/watch?v=l-J6gDYtgFU
-    //Get GPS location
-    //https://www.youtube.com/watch?v=p0PoKEPI65o
-    // Output google maps fragment
+
     private LocationManager locationManager;
-    private FusedLocationProviderClient fusedLocationProviderClient;
     private SupportMapFragment supportMapFragment;
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
         supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.google_map);
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        // If permissions are not granted, ask for location permissions
         if(ContextCompat.checkSelfPermission(MapActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED&&
         ContextCompat.checkSelfPermission(MapActivity.this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(MapActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION,},1);
@@ -55,14 +50,19 @@ public class MapActivity extends AppCompatActivity {
 
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10, 1, new LocationListener() {
             @Override
+            /**
+             *
+             */
             public void onLocationChanged(@NonNull Location location) {
                 supportMapFragment.getMapAsync(new OnMapReadyCallback() {
                     @Override
                     public void onMapReady(GoogleMap googleMap) {
+                        // Get Location of player and put them on the map as a marker
                         LatLng latlng = new LatLng(location.getLatitude(),location.getLongitude());
                         MarkerOptions options = new MarkerOptions().position(latlng).title("Player");
                         googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latlng,10));
                         googleMap.addMarker(options);
+                        // Add all QR codes with location data to the map
                         db.collection("GameCode")
                                 .whereNotEqualTo("latitude",null)
                                 .get()
