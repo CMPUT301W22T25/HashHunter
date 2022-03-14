ab2438e74f2ae0efb57bfedfa8f28500aa8ed9b0
Juan Fernandez <jfernan123@ualberta.ca>
2022-03-13 22:54:22 -0600

    Trying to integrate firestore, not done yet

--- app/src/main/java/com/example/hashhunter/Comment.java
+++ app/src/main/java/com/example/hashhunter/Comment.java
@@ -1,7 +1,52 @@
 package com.example.hashhunter;
 
-public class Comment {
+import android.os.Parcel;
+import android.os.Parcelable;
 
-    //placeholder
+public class Comment implements Parcelable {
+    String userName;
+    String comment;
+    //Constructor for comments
+    Comment(String name, String comment) {
+        this.userName = name;
+        this.comment = comment;
+    }
 
+    protected Comment(Parcel in) {
+        userName = in.readString();
+        comment = in.readString();
+    }
+
+    public static final Creator<Comment> CREATOR = new Creator<Comment>() {
+        @Override
+        public Comment createFromParcel(Parcel in) {
+            return new Comment(in);
+        }
+
+        @Override
+        public Comment[] newArray(int size) {
+            return new Comment[size];
+        }
+    };
+
+    //Gets string representation of comment
+    public String getComment() {
+        return comment;
+    }
+    //Gets username
+    public String getUserName(){
+        return userName;
+    }
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(userName);
+        parcel.writeString(comment);
+    }
 }
--- app/src/main/java/com/example/hashhunter/GameCode.java
+++ app/src/main/java/com/example/hashhunter/GameCode.java
@@ -1,13 +1,17 @@
 package com.example.hashhunter;
 
 import android.location.Location;
+import android.os.Parcel;
+import android.os.Parcelable;
 
 import org.w3c.dom.Comment;
 
 import java.util.ArrayList;
-public class GameCode {
+public class GameCode implements Parcelable {
     private String qrCode;
+    private String title;
     private Photo photo;
+    private Integer treePic;
     private Location location;
     private Integer numPlayers;
     private Integer points;
@@ -15,7 +19,7 @@ public class GameCode {
 
     // DEBUG: I think constructor doesn't need void keyword, this might cause problem later
     // I removed the void keyword while resolving a merge conflict
-    public GameCode(String qrCode, Photo photo, Location location, Integer numPlayers,
+    public GameCode(String qrCode, String title, Integer treePic, Photo photo, Location location, Integer numPlayers,
             Integer points, ArrayList<Comment> commentList) {
         this.qrCode = qrCode;
         this.photo = photo;
@@ -23,6 +27,8 @@ public class GameCode {
         this.numPlayers = numPlayers;
         this.points = points;
         this.commentList = commentList;
+        this.title = title;
+        this.treePic = treePic;
     }
 
     /**
@@ -62,6 +68,37 @@ public class GameCode {
 //        this.location = l;
 //    }
 
+    protected GameCode(Parcel in) {
+        qrCode = in.readString();
+        location = in.readParcelable(Location.class.getClassLoader());
+        if (in.readByte() == 0) {
+            numPlayers = null;
+        } else {
+            numPlayers = in.readInt();
+        }
+        if (in.readByte() == 0) {
+            points = null;
+        } else {
+            points = in.readInt();
+        }
+        title = in.readString();
+        commentList = new ArrayList<>();
+        in.readList(commentList, getClass().getClassLoader());
+
+    }
+
+    public static final Creator<GameCode> CREATOR = new Creator<GameCode>() {
+        @Override
+        public GameCode createFromParcel(Parcel in) {
+            return new GameCode(in);
+        }
+
+        @Override
+        public GameCode[] newArray(int size) {
+            return new GameCode[size];
+        }
+    };
+
     /**
      * Getter for the number of players
      * @return
@@ -97,6 +134,50 @@ public class GameCode {
     public void setPoints(Integer p) {
         this.points = p;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(qrCode);
+        parcel.writeParcelable(location, i);
+        if (numPlayers == null) {
+            parcel.writeByte((byte) 0);
+        } else {
+            parcel.writeByte((byte) 1);
+            parcel.writeInt(numPlayers);
+        }
+        if (points == null) {
+            parcel.writeByte((byte) 0);
+        } else {
+            parcel.writeByte((byte) 1);
+            parcel.writeInt(points);
+        }
+        parcel.writeString(title);
+        parcel.writeList(commentList);
+    }
+    public String getTitle(){
+        return title;
+    }
+    public int getTreePic(){
+        return treePic;
+    }
+    public int getCommentAmount() {
+        return commentList.size();
+    }
+    public Comment getComment(int position){
+        return commentList.get(position);
+    }
+    public void addComment(Comment c){
+        commentList.add(c);
+    }
+    public void setComment(int userName, String commentContent){
+
+    }
+
 
     /**
      * Stores a comment that is about the QR code
