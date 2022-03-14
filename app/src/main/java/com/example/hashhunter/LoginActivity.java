5c8c9e8acc6048f6f7250af1c247aeb41a50e1b3
Gurveer Sohal <gurveersohal433@gmail.com>
2022-03-13 21:37:23 -0600

    started writing tests, and login works

--- app/src/main/java/com/example/hashhunter/LoginActivity.java
+++ app/src/main/java/com/example/hashhunter/LoginActivity.java
@@ -65,19 +65,29 @@ public class LoginActivity extends AppCompatActivity {
                                         SharedPreferences.Editor editor = sharedPreferences.edit();
                                         editor.putString(MainActivity.PREF_UNIQUE_ID, scannedUsername);
                                         editor.commit();
+                                        Button loginButton = findViewById(R.id.login_button);
+                                        loginButton.setText("Logged In");
 
-//                                        Intent intent = new Intent(LoginActivity.this, DashboardActivity.class);
-//                                        startActivity(intent);
+                                        Intent intent = new Intent(LoginActivity.this, DashboardActivity.class);
+                                        startActivity(intent);
                                         // Should this be called?
                                         //finish();
                                     } else {
                                         Toast.makeText(LoginActivity.this, "no username found", Toast.LENGTH_SHORT).show();
+                                        Button loginButton = findViewById(R.id.login_button);
+                                        loginButton.setText("Log In Failed");
                                     }
                                 } else {
                                     Log.d(TAG, "get failed with", task.getException());
+                                    Button loginButton = findViewById(R.id.login_button);
+                                    loginButton.setText("Log In Failed");
                                 }
                             }
                         });
+
+                    } else {
+                        Toast.makeText(LoginActivity.this, "result not ok", Toast.LENGTH_SHORT).show();
+
 
                     }
                 }
@@ -124,7 +134,6 @@ public class LoginActivity extends AppCompatActivity {
                 // https://www.youtube.com/watch?v=7Fc79qTq7yc
                 // I used all the above links to learn about intents and starting activities
                 // The code is not from any single source
-
 
             }
         });
