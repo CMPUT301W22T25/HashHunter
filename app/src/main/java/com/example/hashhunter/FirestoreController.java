f4e0e34e153c9ff093c6c2075cabef6ef39ff0ce
Dimas Sudjito <almuhtad@ualberta.ca>
2022-04-03 13:15:08 -0600

    add javadoc to firestore controller

--- app/src/main/java/com/example/hashhunter/FirestoreController.java
+++ app/src/main/java/com/example/hashhunter/FirestoreController.java
@@ -13,24 +13,43 @@ import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.Map;
 
+/**
+ * A controller class to interact with firestore
+ */
 public class FirestoreController {
     private static FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     /*
     UserInfo collection
      */
-    // read one document from UserInfo collection
+
+    /**
+     * read one document from UserInfo collection
+     * @param userId
+     * @return task document can be read through onCompeleteListener
+     */
     @NonNull
     public static Task<DocumentSnapshot> getUserInfo(String userId) {
         DocumentReference docRef = db.collection("UserInfo").document(userId);
         return docRef.get();
     }
-    // create a new document in UserInfo collection
+
+    /**
+     * create a new document in UserInfo collection
+     * @param userId
+     * @param newUser
+     * @return task can be appended with onCompeleteListener
+     */
     @NonNull
     public static Task<Void> postUserInfo(String userId, Map<String, Object> newUser ) {
         return db.collection("UserInfo").document(userId).set(newUser);
     }
-    // delete a document in UserInfo collection
+
+    /**
+     * delete a document in UserInfo collection
+     * @param userId
+     * @return task can be appended with onCompeleteListener
+     */
     @NonNull
     public static Task<Void> deleteUserInfo(String userId) {
         return db.collection("UserInfo").document(userId).delete();
@@ -39,18 +58,34 @@ public class FirestoreController {
     /*
     Owners collection
      */
-    // read one document from Owners collection
+
+    /**
+     * read one document from Owners collection
+     * @param userId
+     * @return task document can be read through onCompeleteListener
+     */
     @NonNull
     public static Task<DocumentSnapshot> getOwners(String userId) {
         DocumentReference docRef = db.collection("Owners").document(userId);
         return docRef.get();
     }
-    // create a new document in Owners collection
+
+    /**
+     * create a new document in Owners collection
+     * @param userId
+     * @param newUser
+     * @return task can be appended with onCompeleteListener
+     */
     @NonNull
     public static Task<Void> postOwners(String userId, Map<String, Object> newUser ) {
         return db.collection("Owners").document(userId).set(newUser);
     }
-    // delete a document in Owners collection
+
+    /**
+     * delete a document in Owners collection
+     * @param userId
+     * @return task can be appended with onCompeleteListener
+     */
     @NonNull
     public static Task<Void> deleteOwners(String userId) {
         return db.collection("Owners").document(userId).delete();
@@ -59,18 +94,34 @@ public class FirestoreController {
     /*
     Usernames collection
      */
-    // read one document from Usernames collection
+
+    /**
+     * read one document from Usernames collection
+     * @param username
+     * @return task can be appended with onCompeleteListener
+     */
     @NonNull
     public static Task<DocumentSnapshot> getUsernames(String username) {
         DocumentReference docRef = db.collection("Usernames").document(username);
         return docRef.get();
     }
-    // create a new document in Usernames collection
+
+    /**
+     * create a new document in Usernames collection
+     * @param username
+     * @param newUser
+     * @return task can be appended with onCompeleteListener
+     */
     @NonNull
     public static Task<Void> postUsernames(String username, Map<String, Object> newUser) {
         return db.collection("Usernames").document(username).set(newUser);
     }
-    // delete a document in Usernames collection
+
+    /**
+     * delete a document in Usernames collection
+     * @param username
+     * @return task can be appended with onCompeleteListener
+     */
     @NonNull
     public static Task<Void> deleteUsernames(String username) {
         return db.collection("Usernames").document(username).delete();
@@ -79,23 +130,43 @@ public class FirestoreController {
     /*
     Players collection
      */
-    // read one document from Players collection
+
+    /**
+     * // read one document from Players collection
+     * @param userId
+     * @return task can be appended with onCompeleteListener
+     */
     @NonNull
     public static Task<DocumentSnapshot> getPlayers(String userId) {
         DocumentReference docRef = db.collection("Players").document(userId);
         return docRef.get();
     }
-    // create a new document in Players collection
+
+    /**
+     *  // create a new document in Players collection
+     * @param userId
+     * @param player
+     * @return task can be appended with onCompeleteListener
+     */
     @NonNull
     public static Task<Void> postPlayers(String userId, Player player) {
         return db.collection("Players").document(userId).set(player);
     }
-    // delete a document in Players collection
+
+    /**
+     * delete a document in Players collection
+     * @param userId
+     * @return task can be appended with onCompeleteListener
+     */
     @NonNull
     public static Task<Void> deletePlayers(String userId) {
         return db.collection("Players").document(userId).delete();
     }
-    // read all documents from Players collection
+
+    /**
+     * read all documents from Players collection
+     * @return task can be appended with onCompeleteListener
+     */
     @NonNull
     public static Task<QuerySnapshot> getPlayersList() {
         CollectionReference collRef = db.collection("Players");
@@ -105,46 +176,87 @@ public class FirestoreController {
     /*
     GameCode collection
      */
-    // read one document from GameCode collection
+
+    /**
+     * read one document from GameCode collection
+     * @param gameCodeId
+     * @return task can be appended with onCompeleteListener
+     */
     @NonNull
     public static Task<DocumentSnapshot> getGameCode(String gameCodeId) {
         DocumentReference docRef = db.collection("GameCode").document(gameCodeId);
         return docRef.get();
     }
-    // create a new document in GameCode collection
+
+    /**
+     * create a new document in GameCode collection
+     * @param gameCodeId
+     * @param gameCode
+     * @return task can be appended with onCompeleteListener
+     */
     @NonNull
     public static Task<Void> postGameCode(String gameCodeId, GameCode gameCode) {
         return db.collection("GameCode").document(gameCodeId).set(gameCode);
     }
-    // delete a document in GameCode collection
+
+    /**
+     * delete a document in GameCode collection
+     * @param gameCodeId
+     * @return task can be appended with onCompeleteListener
+     */
     @NonNull
     public static Task<Void> deleteGameCode(String gameCodeId) {
         return db.collection("Players").document(gameCodeId).delete();
     }
-    // read all documents from GameCode collection
+
+    /**
+     * read all documents from GameCode collection
+     * @return task can be appended with onCompeleteListener
+     */
     @NonNull
     public static Task<QuerySnapshot> getGameCodeList() {
         CollectionReference collRef = db.collection("GameCode");
         return collRef.get();
     }
-    // get a list of document with locations from GameCode
+
+    /**
+     * get a list of document with locations from GameCode
+     * @return task can be appended with onCompeleteListener
+     */
     @NonNull
     public static Task<QuerySnapshot> getGameCodeListWithLocation() {
         Query collRef = db.collection("GameCode").whereNotEqualTo("latitude",null);
         return collRef.get();
     }
-    // get a list of document of GameCode with a specified title
+
+    /**
+     * get a list of document of GameCode with a specified title
+     * @param title
+     * @return task can be appended with onCompeleteListener
+     */
     @NonNull
     public static Task<QuerySnapshot> getGameCodeListWithTitle(String title) {
         Query collRef = db.collection("GameCode").whereEqualTo("title", title);
         return collRef.get();
     }
-    // get a list of document of GameCode with a specified code
+
+    /**
+     * get a list of document of GameCode with a specified code
+     * @param code
+     * @return task can be appended with onCompeleteListener
+     */
     @NonNull
     public static Task<QuerySnapshot> getGameCodeListWithCode(String code) {
         Query collRef = db.collection("GameCode").whereEqualTo("code", code);
         return collRef.get();
     }
+
+    /**
+     * update a document of GameCode to add a photo
+     * @param gameCodeId
+     * @param photoId
+     * @return task can be appended with onCompeleteListener
+     */
     @NonNull
     public static Task<Void> updateGameCodePhoto(String gameCodeId, String photoId) {
         return db.collection("GameCode").document(gameCodeId).update("photos", FieldValue.arrayUnion(photoId));
@@ -153,23 +265,43 @@ public class FirestoreController {
     /*
     Comment collection
      */
-    // read one document from Comment collection
+
+    /**
+     * read one document from Comment collection
+     * @param commentId
+     * @return task can be appended with onCompeleteListener
+     */
     @NonNull
     public static Task<DocumentSnapshot> getComment(String commentId) {
         DocumentReference docRef = db.collection("Comment").document(commentId);
         return docRef.get();
     }
-    // create a new document in Comment collection
+
+    /**
+     * create a new document in Comment collection
+     * @param commentId
+     * @param newComment
+     * @return task can be appended with onCompeleteListener
+     */
     @NonNull
     public static Task<Void> postComment(String commentId, Map<String, Object> newComment) {
         return db.collection("Comment").document(commentId).set(newComment);
     }
-    // delete a document in Comment collection
+
+    /**
+     * delete a document in Comment collection
+     * @param commentId
+     * @return task can be appended with onCompeleteListener
+     */
     @NonNull
     public static Task<Void> deleteComment(String commentId) {
         return db.collection("Comment").document(commentId).delete();
     }
-    // read all documents from Comment collection
+
+    /**
+     * read all documents from Comment collection
+     * @return task can be appended with onCompeleteListener
+     */
     @NonNull
     public static Task<QuerySnapshot> getCommentList() {
         CollectionReference collRef = db.collection("Comment");
@@ -179,23 +311,43 @@ public class FirestoreController {
     /*
     Photo collection
      */
-    // read one document from Photo collection
+
+    /**
+     * read one document from Photo collection
+     * @param photoId
+     * @return task can be appended with onCompeleteListener
+     */
     @NonNull
     public static Task<DocumentSnapshot> getPhoto(String photoId) {
         DocumentReference docRef = db.collection("Photo").document(photoId);
         return docRef.get();
     }
-    // create a new document in Photo collection
+
+    /**
+     * create a new document in Photo collection
+     * @param photoId
+     * @param newPhoto
+     * @return task can be appended with onCompeleteListener
+     */
     @NonNull
     public static Task<Void> postPhoto(String photoId, Map<String, Object> newPhoto) {
         return db.collection("Photo").document(photoId).set(newPhoto);
     }
-    // delete a document in Photo collection
+
+    /**
+     * delete a document in Photo collection
+     * @param photoId
+     * @return task can be appended with onCompeleteListener
+     */
     @NonNull
     public static Task<Void> deletePhoto(String photoId) {
         return db.collection("Photo").document(photoId).delete();
     }
-    // read all documents from Photo collection
+
+    /**
+     * read all documents from Photo collection
+     * @return task can be appended with onCompeleteListener
+     */
     @NonNull
     public static Task<QuerySnapshot> getPhotoList() {
         CollectionReference collRef = db.collection("Photo");
